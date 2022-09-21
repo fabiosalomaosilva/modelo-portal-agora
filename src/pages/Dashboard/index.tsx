da7bf22020a82bbd47d0034a8465fa63d9ffd839
Fábio Salomão <fabiosalomao@FabioPC.localdomain>
2022-09-21 16:58:23 -0500

    feat:alteração da mensagem inicial

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
     <div className='w-[1230px]'>
     <div className="flex flex-col justify-center align-middle text-center mt-20">
         <div className="h-15 p-10 border-[1px] border-gray-200 rounded-lg shadow-md">
-            <h1 className='mb-5'>Bem vindo ao portal</h1>
+            <h1 className='mb-5'>Bem vindo ao portal de teste</h1>
             <Link to={'/pj/passo01'} className='py-3 px-4 rounded-lg bg-teal-300 text-gray-700 font-normal text-sm hover:bg-teal-200 transition-all ease-in-out'>Cadastro de Pessoa Jurídica</Link>
         </div>
     </div>
